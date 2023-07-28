94327642fc33586015e0910a5a9dd114f0de626f
SLain123 <sl163@ya.ru>
2023-07-28 16:28:19 +0300

    Adds pop-up component (not finished);

---
+++ components/pop-up/Index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{
+    isOpen: boolean;
+}>();
+
+defineEmits<{
+    (e: 'close'): void;
+}>();
+</script>
+
+<template>
+    <div class="layout">
+        <div class="modal-window">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-window {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transition: translate(-50%, -50%);
+    min-width: 300px;
+    height: 200px;
+    padding: 32px;
+}
+</style>
