a8f5221300e09a8bbc657c0f49e61bbd8fd91192
SLain123 <sl163@ya.ru>
2023-08-09 10:31:43 +0300

    Adds card-service component for slider; Adds slides and control buttons in slider;

---
+++ components/card/Service.vue
@@ -0,0 +1,123 @@
+<script lang="ts" setup>
+withDefaults(
+    defineProps<{
+        id: number;
+        imgSrc: string;
+        title: string;
+        descList: string[];
+        bgColor?: string;
+        mode?: 'left' | 'right';
+    }>(),
+    { bgColor: '#4d4d4d', mode: 'left' },
+);
+</script>
+
+<template>
+    <div class="serv_card_container">
+        <img
+            :src="imgSrc"
+            :alt="`slide-${id}`"
+            class="serv_card_img"
+            :class="mode"
+            :width="360"
+            :height="320"
+        />
+        <div class="serv_card_info_block" :style="{ backgroundColor: bgColor }">
+            <div class="serv_card_info_content" :class="mode">
+                <h3 class="serv_card_title">{{ title }}</h3>
+                <ul class="serv_card_list">
+                    <li
+                        v-for="(desc, indx) of descList"
+                        :key="indx"
+                        class="serv_card_desc"
+                    >
+                        {{ desc }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import 'assets/styles/variables.scss';
+
+.serv_card {
+    &_container {
+        width: 100%;
+        display: flex;
+        background-color: transparent;
+        position: relative;
+    }
+
+    &_img {
+        display: block;
+        z-index: 1;
+    }
+
+    &_img.left {
+        margin-left: 24px;
+    }
+
+    &_img.right {
+        margin-left: auto;
+        margin-right: 24px;
+    }
+
+    &_info_block {
+        width: 100%;
+        position: absolute;
+        top: 100px;
+        left: 0;
+        height: calc(100% - 100px);
+        border-radius: 20px;
+    }
+
+    &_info_content {
+        position: absolute;
+        top: 0;
+        padding: 32px 0;
+    }
+
+    &_info_content.left {
+        right: 24px;
+    }
+
+    &_info_content.right {
+        left: 24px;
+    }
+
+    &_title {
+        font-size: 48px;
+        font-style: normal;
+        font-weight: 800;
+        color: white;
+        margin-bottom: 8px;
+    }
+
+    &_list {
+        list-style: none;
+        padding-left: 24px;
+    }
+
+    &_desc {
+        font-family: $hind;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 300;
+        color: white;
+        margin-bottom: 2px;
+        display: flex;
+        align-items: center;
+
+        &:before {
+            content: '';
+            width: 8px;
+            height: 12px;
+            display: inline-block;
+            background-image: url("data:image/svg+xml,%3Csvg width='7' height='11' viewBox='0 0 7 11' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.91211 4.77308C4.5396 4.77308 4.23762 5.07506 4.23762 5.44757C4.23762 5.82008 4.5396 6.12206 4.91211 6.12206V4.77308ZM6.06354 5.92451C6.32694 5.6611 6.32694 5.23404 6.06354 4.97063L1.7711 0.678196C1.5077 0.414791 1.08063 0.414791 0.817226 0.678196C0.553821 0.941602 0.553821 1.36867 0.817226 1.63207L4.63273 5.44757L0.817226 9.26307C0.553821 9.52648 0.553821 9.95354 0.817226 10.2169C1.08063 10.4804 1.5077 10.4804 1.7711 10.2169L6.06354 5.92451ZM4.91211 6.12206H5.5866V4.77308H4.91211V6.12206Z' fill='white'/%3E%3C/svg%3E%0A");
+            margin-right: 4px;
+        }
+    }
+}
+</style>
--- components/home/our-services/Component.vue
+++ components/home/our-services/Component.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation } from 'swiper/modules';
 
 import 'swiper/css';
+import 'swiper/css/navigation';
 </script>
 
 <template>
@@ -15,11 +17,68 @@ import 'swiper/css';
                     color="white"
                 />
             </div>
-            <swiper :slides-per-view="2" :space-between="48">
-                <swiper-slide>Slide 1</swiper-slide>
-                <swiper-slide>Slide 2</swiper-slide>
-                <swiper-slide>Slide 3</swiper-slide>
+
+            <swiper
+                :slides-per-view="1.6"
+                :space-between="42"
+                :modules="[Navigation]"
+                :navigation="{
+                    nextEl: '.o_service_btn_next',
+                    prevEl: '.o_service_btn_prev',
+                }"
+                class="o_service_swiper"
+            >
+                <swiper-slide>
+                    <card-service
+                        :id="0"
+                        imgSrc="assets/img/service-1.png"
+                        title="Accounting"
+                        bgColor="#62C"
+                        mode="left"
+                        :descList="[
+                            'Bookeeping',
+                            'Accounting',
+                            'Bookeeping',
+                            'Accounting',
+                        ]"
+                    />
+                </swiper-slide>
+                <swiper-slide>
+                    <card-service
+                        :id="1"
+                        imgSrc="assets/img/service-2.png"
+                        title="Film & TV"
+                        bgColor="#282828"
+                        mode="right"
+                        :descList="[
+                            'Short films',
+                            'Advertisments',
+                            'Photography',
+                            'Editing',
+                        ]"
+                    />
+                </swiper-slide>
+                <swiper-slide>
+                    <card-service
+                        :id="2"
+                        imgSrc="assets/img/service-3.png"
+                        title="Architecture"
+                        bgColor="#7D1128"
+                        mode="left"
+                        :descList="[
+                            'Bookeeping',
+                            'Accounting',
+                            'Bookeeping',
+                            'Accounting',
+                        ]"
+                    />
+                </swiper-slide>
             </swiper>
+
+            <div class="o_service_control_block">
+                <button type="button" class="o_service_btn_prev"></button>
+                <button type="button" class="o_service_btn_next"></button>
+            </div>
         </wrapper-global>
     </div>
 </template>
@@ -38,6 +97,41 @@ import 'swiper/css';
         display: flex;
         justify-content: space-between;
         align-items: center;
+    }
+
+    &_swiper {
+        position: relative;
+        left: 0;
+        width: calc(113.5%);
+    }
+
+    &_control_block {
+        margin-top: 76px;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    &_btn_prev,
+    &_btn_next {
+        width: 36px;
+        height: 36px;
+        border-radius: 100%;
+        border: none;
+        cursor: pointer;
+        background-color: $m-orange;
+
+        &:hover {
+            opacity: 0.9;
+        }
+    }
+
+    &_btn_prev {
+        background-image: url("data:image/svg+xml,%3Csvg width='36' height='36' viewBox='0 0 50 50' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='24.5286' cy='24.5286' r='24.5286' fill='%23F05B25'/%3E%3Cpath d='M17.9393 25.5892C17.3536 25.0034 17.3536 24.0537 17.9393 23.4679L27.4853 13.922C28.0711 13.3362 29.0208 13.3362 29.6066 13.922C30.1924 14.5077 30.1924 15.4575 29.6066 16.0433L21.1213 24.5286L29.6066 33.0138C30.1924 33.5996 30.1924 34.5494 29.6066 35.1352C29.0208 35.721 28.0711 35.721 27.4853 35.1352L17.9393 25.5892ZM19.7214 26.0286H19V23.0286H19.7214V26.0286Z' fill='white'/%3E%3C/svg%3E%0A");
+        margin-right: 16px;
+    }
+
+    &_btn_next {
+        background-image: url("data:image/svg+xml,%3Csvg width='36' height='36' viewBox='0 0 50 50' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='25.4714' cy='24.5286' r='24.5286' fill='%23F05B25'/%3E%3Cpath d='M32.7821 26.0607C33.3679 25.4749 33.3679 24.5251 32.7821 23.9393L23.2361 14.3934C22.6504 13.8076 21.7006 13.8076 21.1148 14.3934C20.529 14.9792 20.529 15.9289 21.1148 16.5147L29.6001 25L21.1148 33.4853C20.529 34.0711 20.529 35.0208 21.1148 35.6066C21.7006 36.1924 22.6504 36.1924 23.2361 35.6066L32.7821 26.0607ZM31 26.5H31.7214V23.5H31V26.5Z' fill='white'/%3E%3C/svg%3E%0A");
     }
 }
 </style>
